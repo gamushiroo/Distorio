6b827d5c13c108a3c6e95ce8caf9304fbc0a92d8
HeavyXenon <medaka1cocoa1@gmail.com>
2024-11-08 06:04:14 +0900

    ddd

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -17,10 +17,9 @@ public static class PathFinder {
         //  Open the first cell
         open.Add(Vector3Int.zero, new(Vector3Int.zero, 0, 0, 0));
 
-        //  not while(true) because it might do a infinite loop xdddd
         for (int q = 0; q < 10000; q++) {
 
-            //  Find the cell with the lowest F from open cells, and set it to current
+            //  Find the cell with the lowest F from open cells
             KeyValuePair<Vector3Int, Cell> current = init;
             foreach (KeyValuePair<Vector3Int, Cell> entry in open) {
                 if (entry.Value.F < current.Value.F) {
@@ -32,19 +31,18 @@ public static class PathFinder {
             open.Remove(current.Key);
             closed.Add(current.Key, current.Value);
 
-            //  Break if this cell is end
             if (current.Key == localEnd) {
                 break;
             }
 
-            //  Check the neighbour. 3 by 3.
+            //  Check the neighbour
             for (int x = -1; x < 2; x++) {
                 for (int y = -1; y < 2; y++) {
                     for (int z = -1; z < 2; z++) {
 
                         Vector3Int currentNeighbour = new Vector3Int(x, y, z) + current.Key;
 
-                        //  Skip this neighbour if is closed or is unreachable
+                        //  Skip this if is closed or is unreachable
                         if (closed.ContainsKey(currentNeighbour) || world.GetVoxelID(currentNeighbour + start + Vector3Int.down) == 0 || world.GetVoxelID(currentNeighbour + start) != 0) {
                             continue;
                         }
@@ -71,7 +69,7 @@ public static class PathFinder {
             pos = closed[pos].parent;
         }
 
-        //  Generate some stuffs and return it
+        //  Generate stuff
         foreach (Vector3Int _pos in closed.Keys) {
             values.Enqueue(new(Data.Vector3ToChunkVoxel(_pos + start), 19));
         }
@@ -94,11 +92,9 @@ public struct Cell {
     public float F;  //  G + H
 
     public Cell (Vector3Int parent, float G, int H, float F) {
-
         this.parent = parent;
         this.G = G;
         this.H = H;
         this.F = F;
-
     }
 }
