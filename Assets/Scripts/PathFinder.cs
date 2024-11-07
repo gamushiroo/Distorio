6e302e592db01e7484c7344cbaaac73b7231cf86
HeavyXenon <medaka1cocoa1@gmail.com>
2024-11-08 05:56:16 +0900

    Update PathFinder.cs

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public static class PathFinder {
 
+
     //  Cache the results of calls to Sqrt() for performance
     static readonly float[] sqrtValues = new float[3] { 1, Mathf.Sqrt(2), Mathf.Sqrt(3) };
     static readonly KeyValuePair<Vector3Int, Cell> init = new(Vector3Int.zero, new(Vector3Int.zero, Mathf.Infinity, 0, Mathf.Infinity));
@@ -18,8 +19,10 @@ public static class PathFinder {
         //  Open the first cell
         open.Add(Vector3Int.zero, new(Vector3Int.zero, 0, 0, 0));
 
+
         //  not while(true) because it might do a infinite loop xdddd
         for (int q = 0; q < 10000; q++) {
+
 
             //  Find the cell with the lowest F from open cells, and set it to current
             KeyValuePair<Vector3Int, Cell> current = init;
@@ -29,14 +32,17 @@ public static class PathFinder {
                 }
             }
 
+
             //  Close this cell
             open.Remove(current.Key);
             closed.Add(current.Key, current.Value);
+
 
             //  Break if this cell is end
             if (current.Key == localEnd) {
                 break;
             }
+
 
             //  Check the neighbour. 3 by 3.
             for (int x = -1; x < 2; x++) {
@@ -45,6 +51,7 @@ public static class PathFinder {
 
                         Vector3Int currentNeighbour = new Vector3Int(x, y, z) + current.Key;
 
+
                         //  Skip this neighbour if is closed or is unreachable
                         if (closed.ContainsKey(currentNeighbour) || !(world.GetVoxelID(currentNeighbour + start + Vector3Int.down) != 0) || world.GetVoxelID(currentNeighbour + start) != 0) {
                             continue;
@@ -52,7 +59,7 @@ public static class PathFinder {
 
                         Vector3Int lll = localEnd - currentNeighbour;
                         float G = current.Value.G + sqrtValues[new Vector3Int(x, y, z).sqrMagnitude - 1];
-                        float H = Mathf.Abs(lll.x) + Mathf.Abs(lll.y) + Mathf.Abs(lll.z);
+                        int H = Mathf.Abs(lll.x) + Mathf.Abs(lll.y) + Mathf.Abs(lll.z);
                         Cell _ = new(current.Key, G, H, G + H);
 
                         if (!open.ContainsKey(currentNeighbour)) {
@@ -64,6 +71,7 @@ public static class PathFinder {
                 }
             }
         }
+
 
         //  Follow the parent in each cell due to generate the path
         List<Vector3Int> path = new();
@@ -93,10 +101,10 @@ public struct Cell {
 
     public Vector3Int parent;  //  Parent of this cell
     public float G;  //  Steps from the start node till the current node
-    public float H;  //  Heuristic distance from this cell till the end node
+    public int H;  //  Heuristic distance from this cell till the end node
     public float F;  //  G + H
 
-    public Cell (Vector3Int parent, float G, float H, float F) {
+    public Cell (Vector3Int parent, float G, int H, float F) {
 
         this.parent = parent;
         this.G = G;
